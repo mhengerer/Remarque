ba6b36e712d301c4953646e5555cc2458c878f22
DylanF <dylanbrad10@gmail.com>
2023-03-23 00:32:05 -0400

    Server boilerplate

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {};
+
+module.exports = resolvers;
