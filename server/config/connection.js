e5ca5d19e7c5e9a40bd97de69da4f7d23d43d0de
mhengerer <112892304+mhengerer@users.noreply.github.com>
2023-03-30 21:11:48 -0400

    deployment test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/remarque",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/remarque",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/remarque",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
