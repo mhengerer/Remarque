883f303606db8d6e23ade1e51d598c4b2bd814d6
mhengerer <112892304+mhengerer@users.noreply.github.com>
2023-04-02 23:59:27 -0400

    sumtihing

--- client/src/components/Weekday.jsx
+++ client/src/components/Weekday.jsx
@@ -1,22 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
-import {useMutation} from "@apollo/client";
+import { useMutation } from "@apollo/client";
 
-import { UPDATE_PLANNERITEMS } from 
+import { UPDATE_PLANNERITEM } from "../utils/mutations";
 
 // date function
-const Weekday = () => {
+const Weekday = (props) => {
+  const [textArea, setTextArea] = useState("");
+  const [updatePlannerItem, { error }] = useMutation(UPDATE_PLANNERITEM);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const plannerItem = await updatePlannerItem({});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  console.log(JSON.stringify(props));
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setTextArea({ ...textArea, value });
+  };
+
   return (
     <div className="w-full grid grid-cols-1 grid-flow-row text-left border-4 border-base-300">
       <label className="label">
-        <span className=" text-white label-text">Monday 03/25</span>
+        <span className=" text-white label-text">{props.Weekday}</span>
       </label>
       <TextareaAutosize
         type="text"
         minRows="3"
         placeholder="Type here"
         className="flex textarea textarea-bordered w-full resize-none"
-        onChange={}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
       />
     </div>
   );
